9e463d6b20b67252f5c54b0bd494b2dfab079f46
lemon4sour <lemon4sour@gmail.com>
2026-06-24 02:25:51 +0300

    sigint handling

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,41 @@
-import 'dotenv/config';
-import { Client, Events, GatewayIntentBits } from 'discord.js';
+import "dotenv/config";
+import { Client, Events, GatewayIntentBits } from "discord.js";
 
-const client = new Client({ intents: [
-  GatewayIntentBits.Guilds, 
-  GatewayIntentBits.GuildMessages,
-  GatewayIntentBits.MessageContent
-] });
-
-client.on(Events.ClientReady, readyClient => {
-    console.log(`Logged in as ${readyClient.user.tag}!`);
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
 });
 
-client.on(Events.MessageCreate, async message => {
+client.on(Events.ClientReady, (readyClient) => {
+  console.log(`Logged in as ${readyClient.user.tag}!`);
+});
+
+client.on(Events.MessageCreate, async (message) => {
   console.log(message);
-  if (message.content === 'lan') {
-    await message.reply('Lan mı?!');
+  if (message.content === "lan") {
+    await message.reply("Lan mı?!");
   }
 });
 
-client.on(Events.InteractionCreate, async interaction => {
-    if (!interaction.isChatInputCommand()) return;
+client.on(Events.InteractionCreate, async (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
 
-    console.log(interaction);
+  console.log(interaction);
 
-    if (interaction.commandName === 'lan') {
-      await interaction.reply('Lan mı?!');
-    }
+  if (interaction.commandName === "lan") {
+    await interaction.reply("Lan mı?!");
+  }
+});
+
+process.on("SIGINT", async () => {
+  console.log("Goodbye, Ellsworth!");
+
+  client.user?.setStatus("invisible");
+  client.destroy();
+  process.exit(0);
 });
 
 client.login(process.env.TOKEN);
