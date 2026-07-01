423486ddc86973d74d2bb91cd157cf770485abed
lemon4sour <lemon4sour@gmail.com>
2026-07-01 07:40:50 +0300

    forgot to index the command

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -3,4 +3,5 @@ export default [
   (await import("./set-jam-teams-channel.js")).default,
   (await import("./set-jam-cmds-channel.js")).default,
   (await import("./set-jammer-role.js")).default,
+  (await import("./set-guild.js")).default,
 ];
