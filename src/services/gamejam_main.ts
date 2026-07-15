34e7ab89c37986a57a10fddb27d0b98edcd72da6
lemon4sour <lemon4sour@gmail.com>
2026-07-15 07:02:49 +0300

    critical fix: fix new guild state

--- src/services/gamejam_main.ts
+++ src/services/gamejam_main.ts
@@ -15,22 +15,26 @@ export async function setup_gamejam(interaction?: RepliableInteraction) {
   service_ready.lock();
 
   const current_guild = await GamejamData.Guild.get();
-  const new_guild = current_guild?.id === interaction.guild.id;
+  const new_guild = current_guild?.id !== interaction.guild.id;
   GamejamData.Guild.set(interaction.guild);
   console.log("Set guild to", interaction.guild.id);
-  const jammer_role = await createJammerRole(interaction, { new: true });
+  const jammer_role = await createJammerRole(interaction, { new: new_guild });
   console.log("Set jammer role to", jammer_role.id);
-  const operator_role = await createOperatorRole(interaction, { new: true });
+  const operator_role = await createOperatorRole(interaction, {
+    new: new_guild,
+  });
   console.log("Set operator role to", operator_role.id);
-  const channel = await create_panel_channel({ new: true });
+  const channel = await create_panel_channel({ new: new_guild });
   console.log("Set channel to", channel.id);
   const main_panel = await update_panel_message();
   console.log("Set panel message to", main_panel.id);
-  const teams_forum = await create_teams_channel({ new: true });
+  const teams_forum = await create_teams_channel({ new: new_guild });
   console.log("Set team forum to", teams_forum.id);
-  const comm_category = await create_communications_category({ new: true });
+  const comm_category = await create_communications_category({
+    new: new_guild,
+  });
   console.log("Set communications category to", comm_category.id);
-  const archive_category = await create_archive_category({ new: true });
+  const archive_category = await create_archive_category({ new: new_guild });
   console.log("Set archive category to", archive_category.id);
   validate_team_channels();
   console.log("Team instances validated");
