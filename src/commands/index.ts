59d6fc6879cab5a0a4142b1cc3d0d01db167fe71
lemon4sour <lemon4sour@gmail.com>
2026-07-15 04:26:01 +0300

    new op comands indexed

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -3,4 +3,7 @@ export default [
   (await import("./refresh.js")).default,
   (await import("./set-gamejam.js")).default,
   (await import("./add-blacklist.js")).default,
+  (await import("./remove-blacklist.js")).default,
+  (await import("./remove-participant.js")).default,
+  (await import("./remove_from_team.js")).default,
 ];
