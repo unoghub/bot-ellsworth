246a114ad6b47822511bbdbba46b63985004bf2c
FantaLaTone <fantalatone@gmail.com>
2026-08-05 19:21:01 +0300

    added reject cases

--- src/components/register_modal.ts
+++ src/components/register_modal.ts
@@ -1,6 +1,6 @@
 import type { ExtendedClient } from "@/types/client.js";
 import type { Modal } from "@/types/modal.js";
-import { ActionRowBuilder, ButtonBuilder, GuildMember, LabelBuilder, ModalBuilder, ModalSubmitInteraction, PermissionFlagsBits, TextChannel, TextInputBuilder, TextInputStyle, type CacheType, type Channel } from "discord.js";
+import { ActionRowBuilder, ButtonBuilder, GuildMember, LabelBuilder, MessageFlags, ModalBuilder, ModalSubmitInteraction, PermissionFlagsBits, PermissionsBitField, TextChannel, TextInputBuilder, TextInputStyle, type CacheType, type Channel } from "discord.js";
 import Buttons from "./buttons.js";
 import { registryEmbed } from "./embeds.js";
 
@@ -61,12 +61,37 @@ export default {
         ),
     handle: async (client: ExtendedClient, interaction: ModalSubmitInteraction<CacheType>) => {
 
-        const action_row = new ActionRowBuilder<ButtonBuilder>()
-            .addComponents(
-                Buttons.get("registry-approve")!.builder, Buttons.get("registry-reject")!.builder
-            );
+        const action_row = new ActionRowBuilder<ButtonBuilder>().addComponents(
+            Buttons.get("registry-approve")!.builder, Buttons.get("registry-reject")!.builder
+        );
 
         const member = interaction.member as GuildMember;
+
+        let error: string = "";
+
+        const name = interaction.fields.getTextInputValue("name");
+        const email = interaction.fields.getTextInputValue("email");
+        const birthdate = interaction.fields.getTextInputValue("birthdate");
+        const organization = interaction.fields.getTextInputValue("organization");
+        const origin = interaction.fields.getTextInputValue("origin");
+
+        if (name.split(" ").length < 2) {
+            error += "Hatalı ad soyad!";
+        }
+        if (email.toLowerCase().match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/) == null) {
+            error += "Hatalı e-posta!";
+        }
+        if (isNaN(new Date(birthdate).getTime())) {
+            error += "Hatalı doğum tarihi!";
+        }
+
+        if (error != "") {
+            await member.send({
+                content: `Merhaba, yapmış olduğunuz başvuruda sorun vardır. ${error}`,
+            });
+            await interaction.deferUpdate();
+            return;
+        }
 
         const channel = await client.channels.fetch(
             client.config.get("@channels.verification:verification_channel")
@@ -76,14 +101,21 @@ export default {
             embeds: [registryEmbed(
                 client.user?.avatarURL()!,
                 member,
-                interaction.fields.getTextInputValue("name"),
-                interaction.fields.getTextInputValue("email"),
-                interaction.fields.getTextInputValue("birthdate"),
-                interaction.fields.getTextInputValue("organization"),
-                interaction.fields.getTextInputValue("origin"),
+                name,
+                email,
+                birthdate,
+                organization,
+                origin,
             )],
             components: [action_row]
         });
+
+        try {
+            await member.setNickname(interaction.fields.getTextInputValue("name"));
+        } catch (error) {
+            console.error(error);
+        }
+
         interaction.deferUpdate();
     }
 } satisfies Modal;
